a723c62148653cdce7e5ccaa432713588c574748
Ismael <fs.ismael@gmail.com>
2021-12-03 00:13:34 -0300

    Mocking DB access on Get Service to run on docker

--- src/services/GetAllUserService.ts
+++ src/services/GetAllUserService.ts
@@ -2,15 +2,33 @@ import { getRepository } from "typeorm"
 import { User } from "../entities/User"
 
 class GetAllUserService{
+    users = [{
+        id: "123",
+        name: "spider",
+        email: "ui@go.com"
+    } as User,
+    {
+        id: "345",
+        name: "bat",
+        email: "ux@bar.com"
+    } as User]
+    
     async execute() {
-        const repo = getRepository(User);
-
-        const users = await repo.createQueryBuilder('user')
-        .select()
-        .getMany();
-
-        return users;
+        
+        
+        return this.users;
     }
 }
 
 export { GetAllUserService }
+//class GetAllUserService{
+//    async execute() {
+//        const repo = getRepository(User);
+//
+//        const users = await repo.createQueryBuilder('user')
+//        .select()
+//        .getMany();
+//
+//        return users;
+//    }
+//}
